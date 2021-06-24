e04f0cdfdca2f783ecc5fa0a9ed6d86301cd851e
Harriet Craven <hc6@sanger.ac.uk>
2021-06-24 11:34:30 +0100

    SQLALCHEMY_DATABASE_URI is needed in test config

--- cherrytrack/config/test.py
+++ cherrytrack/config/test.py
@@ -14,6 +14,14 @@ TESTING = True
 DATABASE_NAME = "psd_cherrytrack_test"
 
 ###
+# flask-sqlalchemy config
+# https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/
+# This URI needs to be overridden whenever one of the database config (above) changes
+###
+SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER_HOST}:{DATABASE_SERVER_PORT}/{DATABASE_NAME}"
+
+
+###
 # logging config
 ###
 LOGGING["loggers"]["cherrytrack"]["level"] = "DEBUG"
