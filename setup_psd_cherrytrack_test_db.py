e808dfe8eb5691e3087be0393ce038facddfce14
Harriet Craven <hc6@sanger.ac.uk>
2021-07-02 14:35:48 +0100

    playing with CI database init

---
+++ setup_psd_cherrytrack_test_db.py
@@ -0,0 +1,15 @@
+import cherrytrack.config.test as config
+
+create_engine_string = config.SQLALCHEMY_DATABASE_URI
+
+sql_engine = sqlalchemy.create_engine(config.SQLALCHEMY_DATABASE_URI, pool_recycle=3600)
+
+create_db = """
+CREATE DATABASE IF NOT EXISTS `psd_cherrytrack_test` /*!40100 DEFAULT CHARACTER SET latin1 */;
+"""
+
+with sql_engine.connect() as connection:
+    print("*** Creating database ***")
+    connection.execute(create_db)
+
+print("Done")
